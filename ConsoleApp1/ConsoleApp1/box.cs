8d867061670be98c1562d808892d8e0f3801bc24
Andrew <atrybushnyi@kse.org.ua>
2022-02-01 15:06:18 +0200

    alternativeListAdded

--- ConsoleApp1/ConsoleApp1/box.cs
+++ ConsoleApp1/ConsoleApp1/box.cs
@@ -1,19 +1,24 @@
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
 	public class boxes
 	{
 		public Dictionary<int, string> _localDict { get; private set; } = new Dictionary<int, string>();
-		
+		public List<int> alternativeIntList { get; } = new List<int>();
+		public List<string> alternativeStringList { get; } = new List<string>();
 		public void AddElement(int hash, string description)
 		{
 			_localDict.Add(hash, description);
+			alternativeIntList.Add(hash);
+			alternativeStringList.Add(description);
 		}
 		
 		public string GetValue(int key)
 		{
-			return _localDict[key];
+			//return _localDict[key];
+			return alternativeStringList[alternativeIntList.IndexOf(key)];
 		}
 
 		public bool Contains()
